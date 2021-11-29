28e88cb5aec05cdf94dd13146ef7019ae2d922f4
Grigore Stefan <g_stefan@yahoo.com>
2021-11-29 14:47:54 +0200

    Update

--- build/msvc.make.cmd
+++ build/msvc.make.cmd
@@ -28,13 +28,12 @@ if not exist temp\ mkdir temp
 set INCLUDE=%XYO_PATH_REPOSITORY%\include;%INCLUDE%
 set LIB=%XYO_PATH_REPOSITORY%\lib;%LIB%
 set WORKSPACE_PATH=%CD%
+set WORKSPACE_PATH_OUTPUT=%WORKSPACE_PATH%\output
 set WORKSPACE_PATH_BUILD=%WORKSPACE_PATH%\temp
 
 if exist %WORKSPACE_PATH_BUILD%\build.done.flag goto :eof
 
 pushd temp
-
-if not exist apr-util\ mkdir apr-util
 
 rem --- required
 set VENDOR=apr-1.7.0-%XYO_PLATFORM%-dev
@@ -50,7 +49,7 @@ set WEB_LINK=https://github.com/g-stefan/vendor-apr/releases/download/v1.7.0/%VE
 curl --insecure --location %WEB_LINK% --output %VENDOR%.7z
 :apr_process
 7z x -aoa -o. %VENDOR%.7z
-xcopy  /Y /S /E "%VENDOR%\*" "apr-util\"
+xcopy  /Y /S /E "%VENDOR%\*" "..\output\"
 rmdir /Q /S %VENDOR%
 popd
 
@@ -64,9 +63,28 @@ SET CMD_CONFIG=cmake
 SET CMD_CONFIG=%CMD_CONFIG% ../../source
 SET CMD_CONFIG=%CMD_CONFIG% -G "Ninja"
 SET CMD_CONFIG=%CMD_CONFIG% -DCMAKE_BUILD_TYPE=Release
-SET CMD_CONFIG=%CMD_CONFIG% -DCMAKE_INSTALL_PREFIX=%WORKSPACE_PATH_BUILD%\apr-util
+SET CMD_CONFIG=%CMD_CONFIG% -DCMAKE_INSTALL_PREFIX=%WORKSPACE_PATH_OUTPUT%
 SET CMD_CONFIG=%CMD_CONFIG% -DCMAKE_PREFIX_PATH=%XYO_PATH_REPOSITORY%
 
 if not exist %WORKSPACE_PATH_BUILD%\build.configured.flag %CMD_CONFIG%
 if errorlevel 1 goto makeError
 if not exist %WORKSPACE_PATH_BUILD%\build.configured.flag echo configured > %WORKSPACE_PATH_BUILD%\build.configured.flag
+
+ninja
+if errorlevel 1 goto makeError
+ninja install
+if errorlevel 1 goto makeError
+ninja clean
+if errorlevel 1 goto makeError
+
+goto buildDone
+
+:makeError
+popd
+echo "Error: make"
+exit 1
+
+:buildDone
+popd
+echo done > %WORKSPACE_PATH_BUILD%\build.done.flag
+
